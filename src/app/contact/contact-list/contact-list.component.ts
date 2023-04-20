cfd11f816bc87dbcf614322721656fb6b680c6ae
shiv-prakash742 <121678355+shiv-prakash742@users.noreply.github.com>
2023-04-20 17:38:22 +0530

    ..

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/_services/api.service';
-import { AlertService } from '../../_services/alert.service';
+import { AlertService } from 'src/app/_services/alert.service';
 import Swal from 'sweetalert2';
 import { ngxCsv } from 'ngx-csv/ngx-csv';
 
